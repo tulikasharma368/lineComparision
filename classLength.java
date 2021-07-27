162815e0fea1e67aa351d7aeae8ba123a324aeeb
Tulikasharma <tulikasharma368@gmail.com>
2021-07-27 14:32:14 +0530

    UC 1

--- classLength.java
+++ classLength.java
@@ -3,11 +3,13 @@ public class classLength {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Welcome");
 		int x1=1,y1=1,x2=19,y2=1;//line one coordinates
 		int length1=0;
 		length1=(int)(Math.sqrt((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
 		System.out.println("The length of line one is :");
 		System.out.println(length1);
+		
 		
 
 	}
